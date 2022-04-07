3e43130860aeb87b1168c34d57cfa6cd756ff62e
Kyle Sunden <git@ksunden.space>
2022-04-07 17:13:42 -0500

    Spec out properties/config/state

--- yaqd_becker_hickl/_becker_hickl_spc130.py
+++ yaqd_becker_hickl/_becker_hickl_spc130.py
@@ -11,16 +11,60 @@ class BeckerHicklSpc130(IsDaemon):
 
     def __init__(self, name, config, config_filepath):
         super().__init__(name, config, config_filepath)
-        # Perform any unique initialization
+        ...
 
-    async def update_state(self):
-        """Continually monitor and update the current daemon state."""
-        # If there is no state to monitor continuously, delete this function
-        while True:
-            # Perform any updates to internal state
-            self._busy = False
-            # There must be at least one `await` in this loop
-            # This one waits for something to trigger the "busy" state
-            # (Setting `self._busy = True)
-            # Otherwise, you can simply `await asyncio.sleep(0.01)`
-            await self._busy_sig.wait()
+    async def _measure(self):
+        ...
+
+    def get_collect_time(self) -> float:
+        return ...
+
+    def set_collect_time(self, time: float):
+        ...
+
+    def get_collect_time_units(self) -> str:
+        return "s"
+
+    def get_collect_time_limits(self) -> List[float]:
+        return [0.0001, 100000.0]
+
+    def get_tac_range(self) -> float:
+        return ...
+
+    def set_tac_range(self, time: float):
+        ...
+
+    def get_tac_range_units(self) -> str:
+        return "ns"
+
+    def get_tac_range_limits(self) -> List[float]:
+        return [50.0, 5000.0]
+
+    def get_tac_gain(self) -> int:
+        return ...
+
+    def set_tac_gain(self, gain: int):
+        ...
+
+    def get_tac_gain_limits(self) -> List[int]:
+        return [1, 15]
+
+    def get_freq_div(self) -> str:
+        return ...
+
+    def set_freq_div(self, divisor: str):
+        if divisor not in self.get_freq_div_options():
+            raise ValueError(f"{divisor} not in {self.get_freq_div_options()}")
+
+    def get_freq_div_options(self) -> List[str]:
+        return ["1", "2", "4"]
+
+    def get_rate_count_time(self) -> str:
+        return ...
+
+    def set_rate_count_time(self, time: str):
+        if time not in self.get_rate_count_time_options():
+            raise ValueError(f"{time} not in {self.get_rate_count_time_options()}")
+
+    def get_rate_count_time_options(self) -> List[str]:
+        return ["0.05", "0.1", "0.25", "1.0"]
